490063cefb56b1943a182057db0afea2cce75cb4
riskymusic <48032117+riskymusic@users.noreply.github.com>
2019-03-21 13:49:15 +1300

    Week4-class2

---
+++ Task 3 build a rectangle real.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"what symbol do you want to use? ");
+
+            string char1 = Console.ReadLine();
+            Console.WriteLine("what height would you like it to be? ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("what width would you like it to be? ");
+            int width = int.Parse(Console.ReadLine());
+            for (int counter = 0; counter < height; counter++)
+            {
+                for (int counter2 = 0; counter2 < width; counter2++)
+                {
+                    Console.Write(char1);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ Task 3 build rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Write a program that draws a rectangle. 
+            //The width and a height of the rectangle and the inner symbol used to 
+            //construct the rectangle can be pre-set using variables.
+            //Example: Character:/symbol: $ Width: 3 Height: 4 
+        {
+            
+            Console.WriteLine($"Please enter a symbol:  ");
+            string char1 = Console.ReadLine(); //what ever symbol they add in turns into the Char1
+            Console.WriteLine("how high would you like your rectangle to be? ");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("how wide would you like your rectangle to be? ");
+            int width = int.Parse(Console.ReadLine());
+            for (int counter = 0; counter <= height; counter++)
+            {
+                for (int counter2 = 0; counter <= width; counter2++)
+                {
+                    Console.Write(char1);
+                }
+                Console.WriteLine();
+
+                
+                    
+                  
+            }
+            
+        }
+    }
+}
